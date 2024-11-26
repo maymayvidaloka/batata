2ce9e41539ff2353357a86b7d5b0d55674029f58
maymayvidaloka <mayra.ienke2019@gmail.com>
2024-11-26 20:41:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi Dom Pedro II?',
+    'Dom Pedro II foi o segundo imperador do Brasil, governando por quase 50 anos.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é a fórmula de Bhaskara?',
+    'É uma fórmula usada para encontrar as raízes de uma equação do segundo grau.'
+)
+
+criaCartao(
+    'Ciência',
+    'Por que o céu é azul?',
+    'O céu é azul devido ao espalhamento da luz solar pela atmosfera, conhecido como efeito Rayleigh.'
+)
+
+criaCartao(
+    'Música',
+    'Qual é o instrumento mais antigo do mundo?',
+    'A flauta é considerada um dos instrumentos mais antigos, com versões datadas de mais de 35 mil anos.'
+)
